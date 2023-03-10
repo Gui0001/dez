ea541981c7ff247a2b98e36e2dd203de4c8180cb
Gui0001 <117092919+Gui0001@users.noreply.github.com>
2023-03-10 09:36:48 +0100

    Update Javascript portfolio.js

--- Javascript portfolio.js
+++ Javascript portfolio.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
     $(window).scroll(function () {
-      // ------------------------------ sticky navbar on scroll script ------------------------------ //
       if (this.scrollY > 20) {
         $(".navbar").addClass("sticky");
       } else {
         $(".navbar").removeClass("sticky");
       }
   
-      // ------------------------------ scroll-up button show/hide script ------------------------------ //
       if (this.scrollY > 500) {
         $(".scroll-up-btn").addClass("show");
       } else {
@@ -15,44 +13,38 @@ $(document).ready(function () {
       }
     });
   
-    // ------------------------------ slide-up script ------------------------------ //
   
     $(".scroll-up-btn").click(function () {
       $("html").animate({ scrollTop: 0 });
-      // ------------------------------ removing smooth scroll on slide-up button click ------------------------------ //
       $("html").css("scrollBehavior", "auto");
     });
   
     $(".navbar .menu li a").click(function () {
-      // ------------------------------ Smooth scroll on Menu Items click ------------------------------ //
   
       $("html").css("scrollBehavior", "smooth");
     });
   
-    // ------------------------------ Toggle Navbar ------------------------------ //
   
     $(".menu-btn").click(function () {
       $(".navbar .menu").toggleClass("active");
       $(".menu-btn i").toggleClass("active");
     });
   
-    // ------------------------------ Typing Text Animation ------------------------------ //
   
     var typed = new Typed(".typing", {
-      strings: ["Frontend Dev","Backend Dev", "Designer"],
+      strings: ["Frontend Dev","Backend Dev",],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
     });
   
     var typed = new Typed(".typing-2", {
-      strings: ["Frontend Dev","Backend Dev", "Designer"],
+      strings: ["Frontend Dev", "Backend Dev"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
     });
   
-    // ------------------------------ Owl Carousel ------------------------------ //
   
     $(".carousel").owlCarousel({
       margin: 20,
